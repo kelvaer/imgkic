5e00ac7197da6a3c2b9b7f4bd0c6503858edc902
hang2023 <hang2023@aliyun.com>
2024-06-13 14:28:45 +0800

    test json api

--- src/main/java/com/example/imgkic/DemoController.java
+++ src/main/java/com/example/imgkic/DemoController.java
@@ -1,8 +1,11 @@
 package com.example.imgkic;
 
 import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Get;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.annotation.Param;
+import org.noear.solon.annotation.Produces;
+import org.noear.solon.boot.web.MimeType;
 
 @Controller
 public class DemoController {
@@ -10,4 +13,14 @@ public class DemoController {
     public String hello(@Param(defaultValue = "world") String name) {
         return String.format("Hello %s!", name);
     }
+
+    @Produces(MimeType.APPLICATION_JSON_VALUE)
+    @Get
+    @Mapping("/test2")
+    public String test2(){
+        return "{\"message\":\"返回字符串并输出\"}";
+    }
+
+
+
 }
